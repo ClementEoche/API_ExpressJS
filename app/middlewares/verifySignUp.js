e0699da3439c7625bcd55a57b141aa32b84aec20
ClementEoche <clement.eoche35@gmail.com>
2021-05-06 14:22:59 +0200

    zafzeafzaegaezgzeag

--- app/middlewares/verifySignUp.js
+++ app/middlewares/verifySignUp.js
@@ -2,7 +2,7 @@ const db = require("../models");
 const ROLES = db.ROLES;
 const User = db.user;
 
-checkDuplicateUsername = (req, res, next) => {
+checkDuplicateUsernameOrEmail = (req, res, next) => {
   // Username
   User.findOne({
     username: req.body.username
@@ -16,6 +16,23 @@ checkDuplicateUsername = (req, res, next) => {
       res.status(400).send({ message: "Failed! Username is already in use!" });
       return;
     }
+
+    // Email
+    User.findOne({
+      email: req.body.email
+    }).exec((err, user) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+
+      if (user) {
+        res.status(400).send({ message: "Failed! Email is already in use!" });
+        return;
+      }
+
+      next();
+    });
   });
 };
 
@@ -35,7 +52,7 @@ checkRolesExisted = (req, res, next) => {
 };
 
 const verifySignUp = {
-  checkDuplicateUsername,
+  checkDuplicateUsernameOrEmail,
   checkRolesExisted
 };
 
--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -12,7 +12,7 @@ module.exports = function(app) {
   app.post(
     "/api/auth/signup",
     [
-      verifySignUp.checkDuplicateUsername,
+      verifySignUp.checkDuplicateUsernameOrEmail,
       verifySignUp.checkRolesExisted
     ],
     controller.signup
